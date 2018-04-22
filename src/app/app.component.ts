6e457a92fc822f44a284f1a7b97c59633800fc0d
Kizilov <kizilov.yura@gmail.com>
2018-04-22 20:13:54 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    url = 'https://html.art-craft.xyz/testServer/db.json';
+    data;
+    id;
+    name;
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.getServer();
+        // this.getData();
+    }
+
+    getServer() {
+        this.http.get(this.url).subscribe(data => {
+            console.log(data);
+        });
+    }
+
+    getData() {
+        this.http.get(this.url + '/somedata').subscribe(data => {
+            this.data = data;
+            console.log(data);
+        });
+    }
+    pushData() {
+        const _data = {
+            'id': this.id,
+            'name': this.name
+        };
+        return this.http.post(this.url + '/somedata', _data).subscribe(data => {
+            console.log(data);
+            this.getData();
+        });
+    }
 }
